d6dbdcd2ae8e32f6a113cc1c8c49aaf65a818968
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:15:00 +0000

    Python - Variable Annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Function add: returns the sum of two floats
+Function add: returns the sum of two floats.
 """
+
 
 def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
