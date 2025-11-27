64acd073c8ec1597bcb7fe4e242e9f451db932e2
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:43:10 +0000

    Python - Variable Annotations

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -8,6 +8,7 @@ from typing import List, Union
 # def inp(a: List[Union[float, int]]) -> float:
 #     return sum(a)
 
-def inp(a: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return the sum of all elements in mxd_lst as a float."""
-    return sum(a)
+    return sum(mxd_lst)
+
