bfad81bf1790f3d29f3ac2c67437d12bc00889c4
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:38:06 +0000

    Python - Variable Annotations

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Function sum_list: returns the sum of a list of floats.
+"""
+
+from typing import List
+
+# def inp(a: List[float]) -> float:
+#     return sum(a)
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of all elements in input_list."""
+    return sum(input_list)
