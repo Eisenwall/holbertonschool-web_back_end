53e7869338e6245dbfa30ef742e586112b55fad6
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:14:04 +0000

    Python - Variable Annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
-def inp(a: float, b: float) -> float:
+"""
+Function add: returns the sum of two floats
+"""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b."""
     return a + b
