5ed9c6e7867fc441d9619604a5a4cb6e77fb0142
Eisenwall <Eisenwall@proton.me>
2025-11-27 10:15:28 +0000

    Python - Variable Annotations

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Function make_multiplier: returns a function that multiplies a float
+by the given multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies its argument by multiplier."""
+
+    def nb(n: float) -> float:
+        return n * multiplier
+
+    return nb
