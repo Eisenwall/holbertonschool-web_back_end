5e3134f97213a97238848fd5ac8546393348f8e6
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:20:17 +0000

    Python - Variable Annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function concat: returns the concatenation of two strings.
+"""
+
+# def inp(str1: str, str2: str) -> str:
+#     return str1 + str2
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2."""
+    return str1 + str2
