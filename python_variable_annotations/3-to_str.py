2d84c16889c61e153c1eea85cbdf5f445cd80b44
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:26:12 +0000

    Python - Variable Annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Function to_str: returns a float as a string.
+"""
+
+import math
+
+# def inp(n: float) -> str:
+#     return str(n)
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float n."""
+    return str(n)
