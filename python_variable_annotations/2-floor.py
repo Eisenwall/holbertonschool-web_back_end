00485fecce9ca527f0bdb64f783a2242f3dbbf16
Eisenwall <Eisenwall@proton.me>
2025-11-27 09:24:38 +0000

    Python - Variable Annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Function floor: returns the floor of a float.
+"""
+
+import math
+
+# def inp(n: float) -> int:
+#     return math.floor(n)
+
+
+def floor(n: float) -> int:
+    """Return the floor of n."""
+    return math.floor(n)
