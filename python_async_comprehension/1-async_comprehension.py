6a0ee75a2f19906682cc13348427dbdd607edc1c
Eisenwall <Eisenwall@proton.me>
2025-11-30 15:15:24 +0000

    HTS

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-''' Async Comprehension '''
+"""Async Comprehension"""
+
 from typing import List
-l = List[float]
 
-foc = __import__('0-async_generator').async_generator
+Vector = List[float]
+
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> l:
-    ''' Function that return a list '''
-    d = [s async for s in foc()]
-    return d
+async def async_comprehension() -> Vector:
+    """Function that returns a list of 10 random numbers using async comprehension."""
+    numbers = [
+        value
+        async for value in async_generator()
+    ]
+    return numbers
