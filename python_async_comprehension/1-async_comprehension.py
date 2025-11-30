7f7f0dcb9d0bc27a44cad64fa5a0661021d29865
Eisenwall <Eisenwall@proton.me>
2025-11-30 15:11:23 +0000

    HTS

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,15 @@
 
 from typing import List
 
+Vector = List[float]
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    """Collect 10 random numbers using an async comprehension over async_generator."""
-    return [
-        i
-        async for i in async_generator()
+async def async_comprehension() -> Vector:
+    """Collect 10 random numbers using async comprehension from async_generator."""
+    numbers: Vector = [
+        number
+        async for number in async_generator()
     ]
+    return numbers
