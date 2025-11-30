668c21ec31617bf2b63076a3b58bf3a6e7e83322
Eisenwall <Eisenwall@proton.me>
2025-11-30 15:26:34 +0000

    HTS

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,11 +6,13 @@ import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-
 async def measure_runtime() -> float:
     """Measure total runtime of four async_comprehension coroutines in parallel."""
     start = time.perf_counter()
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                         async_comprehension(), async_comprehension())
+    tasks = [
+        async_comprehension(), async_comprehension(),
+        async_comprehension(), async_comprehension()
+    ]
+    await asyncio.gather(*tasks)
     end = time.perf_counter()
     return end - start
