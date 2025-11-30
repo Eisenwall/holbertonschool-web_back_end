f7b76960067b892c80d3c8b5c330fa496fdecd6c
Eisenwall <Eisenwall@proton.me>
2025-11-30 14:32:49 +0000

    HTS

---
+++ 0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async generator that yields 10 random numbers with a 1-second delay"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Coroutine that asynchronously yields a random number between 0 and 10
+    every second for 10 iterations.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
