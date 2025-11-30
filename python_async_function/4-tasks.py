90f2ebe04bea57528229ab0281a57d0224b76a6f
Eisenwall <Eisenwall@proton.me>
2025-11-30 14:25:24 +0000

    HTS

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-"""Module 4-tasks
-Contains an asynchronous coroutine task_wait_n that spawns
-task_wait_random n times and returns a list of delays in ascending order.
-"""
-
+''' async and await syntax '''
 import asyncio
+from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+get = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
-    """Spawn task_wait_random n times with the specified max_delay.
-    Return the list of delays in ascending order (without using sort).
-    """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = [await task for task in asyncio.as_completed(tasks)]
-    return delays
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    ''' Function that returns a list
+    '''
+    l = [get(max_delay) for i in range(n)]
+    finish = [await task for task in asyncio.as_completed(l)]
+    return finish
