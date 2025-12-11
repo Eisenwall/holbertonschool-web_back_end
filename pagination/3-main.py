a3a601bc0eb04336fe08b82fd2457c518bc2ff38
Eisenwall <Eisenwall@proton.me>
2025-12-11 16:36:13 +0400

    Clean up main file for testing get_hyper_index
    Refactored print statements for clarity and consistency.

--- pagination/3-main.py
+++ pagination/3-main.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 """
-Main file
+Cleaned-up main file for testing get_hyper_index
 """
 
 Server = __import__('3-hypermedia_del_pagination').Server
 
 server = Server()
-
 server.indexed_dataset()
-
 try:
     server.get_hyper_index(300000, 100)
 except AssertionError:
-    print("AssertionError raised when out of range")        
-
-
+    print("AssertionError raised when out of range")
 index = 3
 page_size = 2
 
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+print("Nb items:", len(server._Server__indexed_dataset))
 res = server.get_hyper_index(index, page_size)
 print(res)
-print(server.get_hyper_index(res.get('next_index'), page_size))
-del server._Server__indexed_dataset[res.get('index')]
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+res_next = server.get_hyper_index(res["next_index"], page_size)
+print(res_next)
+del server._Server__indexed_dataset[res["index"]]
+print("Nb items:", len(server._Server__indexed_dataset))
 print(server.get_hyper_index(index, page_size))
-print(server.get_hyper_index(res.get('next_index'), page_size))
+print(server.get_hyper_index(res["next_index"], page_size))
