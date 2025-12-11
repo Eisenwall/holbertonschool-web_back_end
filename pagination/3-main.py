bbed1ac9e85e53eb9be1a1ca358bfa3c77de97b4
Eisenwall <Eisenwall@proton.me>
2025-12-11 16:42:26 +0400

    Refactor main file for hypermedia pagination testing

--- pagination/3-main.py
+++ pagination/3-main.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
 """
-Cleaned-up main file for testing get_hyper_index
+Main file
 """
 
 Server = __import__('3-hypermedia_del_pagination').Server
 
 server = Server()
+
 server.indexed_dataset()
+
 try:
     server.get_hyper_index(300000, 100)
 except AssertionError:
     print("AssertionError raised when out of range")
+
+
 index = 3
 page_size = 2
 
-print("Nb items:", len(server._Server__indexed_dataset))
+print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+# 1- request first index
 res = server.get_hyper_index(index, page_size)
 print(res)
-res_next = server.get_hyper_index(res["next_index"], page_size)
-print(res_next)
-del server._Server__indexed_dataset[res["index"]]
-print("Nb items:", len(server._Server__indexed_dataset))
+
+# 2- request next index
+print(server.get_hyper_index(res.get('next_index'), page_size))
+
+# 3- remove the first index
+del server._Server__indexed_dataset[res.get('index')]
+print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+# 4- request again the initial index -> the first data retreives is not the same as the first request
 print(server.get_hyper_index(index, page_size))
-print(server.get_hyper_index(res["next_index"], page_size))
+
+# 5- request again initial next index -> same data page as the request 2-
+print(server.get_hyper_index(res.get('next_index'), page_size))
