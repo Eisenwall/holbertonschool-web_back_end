125eae1badba5c5bfbd0838361fb1e6d54943f1b
Your Name <Eisenwall@proton.me>
2025-12-20 08:47:15 +0000

    HTS

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,22 +6,22 @@ Script that provides stats about Nginx logs stored in MongoDB
 
 from pymongo import MongoClient
 
-def main():
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
 
+    # total number of logs
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
-    print("Methods:")
+    # methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
+    # number of GET /status
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
-
-if __name__ == "__main__":
-    main()
