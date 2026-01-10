c1149bce5906e8e786968589e49f77fcfe12a38a
Eisenwall <Eisenwall@proton.me>
2026-01-10 16:16:40 +0000

    HTS

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
 export default function cleanSet(set, startString = '') {
   const result = [...set]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
-    .join('-');
+    .filter(value => typeof value === 'string' && value.startsWith(startString))  // Check that the value is a string
+    .map(value => value.slice(startString.length))  // Remove startString from the value
+    .join('-');  // Join the results with '-'
 
   return result;
 }
-
