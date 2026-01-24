2a2617ac5a23f5d20d7c2b37cb17aa62ec71710a
Eisenwall <Eisenwall@proton.me>
2026-01-24 08:07:17 +0000

    HTS

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${data.trim()}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('\nThis important software is now closing\n');
 });
